66fa6464e408f49dfdea615d66b5a7cb8d059ab3
Dmitrii Kolpakov <dmitrii.kolpakov@vk.team>
2024-04-13 11:38:00 +0300

    Rename "run_me.sh" -> "download_listings.sh"

--- MIAP/download_listings.sh
+++ MIAP/download_listings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DATASET_SPLIT=$1
-CORES_NUM=$2
 
 python downloader.py open_images_extended_miap_images_${DATASET_SPLIT}.lst \
        --download_folder=./MIAP/${DATASET_SPLIT}/images/
