7f2e9e86097a8650546a752e1acc16a9359e1d1f
SharkovKirill <kirill.office75@gmail.com>
2024-04-17 14:32:12 +0300

    code for new dataset

--- EDA.py
+++ EDA.py
@@ -31,6 +31,28 @@ show_bb_yolo(
     figsize=(40, 20),
 )
 
+show_bb_yolo(
+    "COCO",
+    "./coco-2017/train/",
+    cols=5,
+    rows=2,
+    shuffle=True,
+    data_type="train",
+    figsize=(40, 20),
+)
+
+show_bb_yolo(
+    "COCO",
+    "./coco-2017/valid/",
+    cols=5,
+    rows=2,
+    shuffle=True,
+    data_type="valid",
+    figsize=(40, 20),
+)
+
 save_hists("KITTI", "./Kitti/train/", "train", figsize=(15, 6))
 save_hists("VOC", "./Pascal-VOC-2012-1/train/", "train", figsize=(15, 6))
-save_hists("VOC", "./Pascal-VOC-2012-1/valid/", "train", figsize=(15, 6))
+save_hists("VOC", "./Pascal-VOC-2012-1/valid/", "valid", figsize=(15, 6))
+save_hists("COCO", "./coco-2017/train/", "train", figsize=(15, 6))
+save_hists("COCO", "./coco-2017/valid/", "valid", figsize=(15, 6))
