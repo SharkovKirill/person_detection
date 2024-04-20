055a08bd762fd961e8413af39784adb1e60db3bd
SharkovKirill <kirill.office75@gmail.com>
2024-04-20 13:48:48 +0300

    refactoring

--- EDA.py
+++ EDA.py
@@ -10,79 +10,58 @@ show_bb_yolo(
     figsize=(40, 20),
     pictures_type=".png",
 )
-
-show_bb_yolo(
-    "VOC",
-    "./Pascal-VOC-2012-1/train/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="train",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "VOC",
-    "./Pascal-VOC-2012-1/valid/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="valid",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "COCO",
-    "./coco-2017/train/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="train",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "COCO",
-    "./coco-2017/valid/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="valid",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "WiderPerson",
-    "./WiderPerson-3/valid/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="valid",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "WiderPerson",
-    "./WiderPerson-3/test/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="test",
-    figsize=(40, 20),
-)
-
-show_bb_yolo(
-    "WiderPerson",
-    "./WiderPerson-3/train/",
-    cols=5,
-    rows=2,
-    shuffle=True,
-    data_type="train",
-    figsize=(40, 20),
-)
-
 save_hists("KITTI", "./Kitti/train/", "train", figsize=(15, 6))
-save_hists("VOC", "./Pascal-VOC-2012-1/train/", "train", figsize=(15, 6))
-save_hists("VOC", "./Pascal-VOC-2012-1/valid/", "valid", figsize=(15, 6))
-save_hists("COCO", "./coco-2017/train/", "train", figsize=(15, 6))
-save_hists("COCO", "./coco-2017/valid/", "valid", figsize=(15, 6))
+
+
+for subset in ["train", "valid"]:
+    show_bb_yolo(
+        "VOC",
+        f"./Pascal-VOC-2012-1/{subset}/",
+        cols=5,
+        rows=2,
+        shuffle=True,
+        data_type=subset,
+        figsize=(40, 20),
+    )
+    save_hists("VOC", f"./Pascal-VOC-2012-1/{subset}/", subset, figsize=(15, 6))
+
+
+for subset in ["train", "valid"]:
+    show_bb_yolo(
+        "COCO",
+        f"./coco-2017/{subset}/",
+        cols=5,
+        rows=2,
+        shuffle=True,
+        data_type=subset,
+        figsize=(40, 20),
+    )
+    save_hists("COCO", f"./coco-2017/{subset}/", subset, figsize=(15, 6))
+
+
+# for subset in ["train", "valid", "test"]:
+#     show_bb_yolo(
+#         "WiderPerson",
+#         f"./WiderPerson-3/{subset}/",
+#         cols=5,
+#         rows=2,
+#         shuffle=True,
+#         data_type=subset,
+#         figsize=(40, 20),
+#     )
+#     save_hists("WiderPerson", f"./WiderPerson-3/{subset}/", subset, figsize=(15, 6))
+
+
+for subset in ["train", "valid", "test"]:
+    show_bb_yolo(
+        "open-images-v7",
+        f"./open-images-v7/{subset}/",
+        cols=5,
+        rows=2,
+        shuffle=True,
+        data_type=subset,
+        figsize=(40, 20),
+    )
+    save_hists(
+        "open-images-v7", f"./open-images-v7/{subset}/", subset, figsize=(15, 6)
+    )
